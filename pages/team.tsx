0aef79b2094d9ffeb7ed005474aa90252e22eaa6
Arno van Staden <arnovanstaden@gmail.com>
2021-04-26 10:49:54 +0200

    update team

--- pages/team.tsx
+++ pages/team.tsx
@@ -16,7 +16,7 @@ const team = () => {
         <Page
             className={styles.team}
             head={{
-                title: "Our Team| BHR Solutions",
+                title: "Our Team | BHR Solutions",
                 description: "FIX THIS",
                 canonical: "/team"
             }}
@@ -29,12 +29,172 @@ const team = () => {
             <Container >
                 <section className={styles.intro}>
                     <div className="heading">
-                        <h1>Industries</h1>
-                        <h2>Our solutions across the building, hospitality and retail industries helps to save energy, reduce costs, improve operational longevity and increase our client’s operator and customer comfort.</h2>
+                        <h1>A people-oriented approach, based on trust,                     openness and collaboration. <br />
+                            <span> Including everyone for the best solution.</span>
+                        </h1>
                     </div>
                 </section>
 
+                <section className={styles.training}>
+                    <h1>Training and Development</h1>
+                    <p>Through training and development we equip our personnel to make the right decisions for the long-term success of our clients and our business.</p>
+                    <NextImage src="/images/pages/team/training1.jpg" alt="BHR Training and Development" />
+
+                    <small>
+                        Image caption and description. Phasellus sagittis vel mauris nec pretium. n elementum nibh scelerisque sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    </small>
+
+                    <Grid container className={styles.grid}>
+                        <Grid item md={6}>
+                            <div className={styles.initiatives}>
+                                <h3>Training and Development Initiatives</h3>
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                                </ul>
+                            </div>
+                        </Grid>
+                        <Grid item md={6}>
+                            <div className={styles.image}>
+                                <NextImage src="/images/pages/team/training2.jpg" alt="BHR Training and Development" />
+                            </div>
+                        </Grid>
+                    </Grid>
+                </section>
+
+                <section className={styles.inclusion}>
+                    <h1>Inclusion and Diversity</h1>
+                    <p>
+                        At BHR we create an environment where people of all backgrounds can come
+                        together and achieve success.
+                       
+                    </p>
+
+                    <h5>
+                        BHR SOLUTIONS is a Level two B-BBEE contributor
+                    </h5>
+
+                    <Grid container spacing={5}>
+                        <Grid item md={6}>
+                            <div className={styles.contentBlock}>
+                                <div className={styles.image}>
+                                    <NextImage src="/images/pages/team/inclusion1.jpg" alt="BHR Inclusion and Diversity" />
+                                    <div className={styles.member}>
+                                        <h6>Dieu-Merci</h6>
+                                        <p>Electrician</p>
+                                    </div>
+                                </div>
+                                <div className={styles.text}>
+                                    <p>
+                                    “Donec quis ante ut justo pharetra rhoncus sed id velit. Etiam vitae erat luctus, scelerisque leo eu, faucibus ligula.”
+                                    </p>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item md={6}>
+                            <div className={styles.contentBlock}>
+                                <div className={styles.image}>
+                                    <NextImage src="/images/pages/team/inclusion2.jpg" alt="BHR Inclusion and Diversity" />
+                                    <div className={styles.member}>
+                                        <h6>Raymond</h6>
+                                        <p>Project Manager</p>
+                                    </div>
+                                </div>
+                                <div className={styles.text}>
+                                    <p>
+                                    “Donec quis ante ut justo pharetra rhoncus sed id velit. Etiam vitae erat luctus, scelerisque leo eu, faucibus ligula.”
+                                    </p>
+                                </div>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </section>
+
+                <section className={styles.expertise}>
+                    <h1>BHR Team Expertise</h1>
+
+                    <Grid container className={styles.quoteGrid} spacing={5}>
+                        <Grid item md={6}>
+                            <div className={styles.quote}>
+                                <p>“Etiam vitae erat luctus,
+                                scelerisque leo eu, faucibus
+                                ligula. Donec quis ante ut
+                                justo pharetra rhoncus
+                                sed id velit.”</p>
+                                <h6>Hennie</h6>
+                                <small>Director</small>
+                            </div>
+                        </Grid>
+                        <Grid item md={6}>
+                            <div className={styles.image}>
+                            <NextImage src="/images/pages/team/hennie.jpg" alt="BHR Team Expertise" />
+                            </div>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container className={styles.textGrid} spacing={5}>
+                        <Grid item md={4}>
+                            <div className={styles.text}>
+                                <h5>Management</h5>
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                </ul>
+                            </div>
+                        </Grid>
+                        <Grid item md={4}>
+                            <div className={styles.text}>
+                                <h5>Administrative</h5>
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                </ul>
+                            </div>
+                        </Grid>
+                        <Grid item md={4}>
+                            <div className={styles.text}>
+                                <h5>Operational</h5>
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                    <li>Lorem ipsum dolor sit amet.</li>
+                                </ul>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </section>
+
+                <section className={styles.structure}>
+                    <h1>Operational Structure</h1>
+
+                    <Grid container className={styles.grid}>
+                        <Grid item md={6}>
+                            <a href="">
+                                Download PDF
+                            </a>
+                        </Grid>
+                        <Grid item md={6}>
+                            <div className={styles.image}>
+                            <img src="/images/pages/team/structure.svg" alt=""/>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </section>
+
             </Container>
+
+
+
 
         </Page>
     )
