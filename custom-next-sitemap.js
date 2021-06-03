afba058fea81c06b001d25bb490fd7b12ac0985a
Arno van Staden <arnovanstaden@gmail.com>
2021-06-03 12:45:08 +0200

    update sitemap

---
+++ custom-next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: "https://www.bhrsolutions.co.za",
+    generateRobotsTxt: true, // (optional),
+    exclude: ["/404", "/cart", "/cart*"],
+    changefreq: 'daily',
+    priority: 0.8,
+    sitemapSize: 7000,
+}
